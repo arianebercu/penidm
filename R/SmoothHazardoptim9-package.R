22c304bbae96c9a03483151117f12b0133692991
arianebercu <82817493+arianebercu@users.noreply.github.com>
2023-09-21 14:33:29 +0200

    check ex

--- R/SmoothHazardoptim9-package.R
+++ R/SmoothHazardoptim9-package.R
@@ -1,6 +1,23 @@
-#' @keywords internal
-"_PACKAGE"
 
-## usethis namespace: start
-## usethis namespace: end
+
+#' Data set for survival models: right-censored and interval-censored data.
+#' 
+#' A simulated data frame for survival models composed of right-censored and
+#' interval-censored data.
+#' 
+#' 
+#' @name testdata
+#' @docType data
+#' @format A data frame with 936 observations on the following 4 variables.
+#' \describe{ \item{l}{for diseased subjects: left endpoint of
+#' censoring interval; for non-diseased subjects: right censoring time}
+#' \item{r}{for diseased subjects: right endpoint of censoring
+#' interval; for non-diseased subjects: right censoring time for the disease
+#' event} \item{id}{disease status} \item{cov}{covariate} }
+#' @keywords datasets
+#' @examples
+#' 
+#' data(testdata)
+#' head(testdata)
+#' 
 NULL
