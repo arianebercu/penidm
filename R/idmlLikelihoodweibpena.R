65b596b1bc5371b295f25f92ca5599f5fb1329eb
arianebercu <82817493+arianebercu@users.noreply.github.com>
2023-09-22 15:39:42 +0200

    lambda[, change

--- R/idmlLikelihoodweibpena.R
+++ R/idmlLikelihoodweibpena.R
@@ -50,8 +50,7 @@ idmlLikelihoodweibpena<-function(b0,np0,npar0,bfix0,fix0,c0,no0,ve010,ve020,ve12
     res<-res+lambda[,3]*alpha*sum(abs(b12))+lambda[,3]*(1-alpha)*sum(b12*b12)
   }
   if(penalty=="mcp"){
-    
-<<<<<<< HEAD
+
     p01<-rep(alpha*lambda[,1]*lambda[,1]/2,length(b01))
     idbeta<-which(b01<=alpha*lambda[,1])
     p01[idbeta]<-lambda[,1]*abs(b01[idbeta])-((b01[idbeta]*b01[idbeta])/2*alpha)
@@ -63,28 +62,26 @@ idmlLikelihoodweibpena<-function(b0,np0,npar0,bfix0,fix0,c0,no0,ve010,ve020,ve12
     p12<-rep(alpha*lambda[,3]*lambda[,3]/2,length(b12))
     idbeta<-which(b12<=alpha*lambda[,3])
     p12[idbeta]<-lambda[,3]*abs(b12[idbeta])-((b12[idbeta]*b12[idbeta])/2*alpha)
-=======
-    p01<-rep(alpha*lambda[1]*lambda[1]/2,length(b01))
-    idbeta<-which(b01<=alpha*lambda[1])
-    p01[idbeta]<-lambda[1]*abs(b01[idbeta])-((b01[idbeta]*b01[idbeta])/2*alpha)
+
+    p01<-rep(alpha*lambda[,1]*lambda[,1]/2,length(b01))
+    idbeta<-which(b01<=alpha*lambda[,1])
+    p01[idbeta]<-lambda[,1]*abs(b01[idbeta])-((b01[idbeta]*b01[idbeta])/2*alpha)
     
-    p02<-rep(alpha*lambda[2]*lambda[2]/2,length(b02))
-    idbeta<-which(b02<=alpha*lambda[2])
-    p02[idbeta]<-lambda[2]*abs(b02[idbeta])-((b02[idbeta]*b02[idbeta])/2*alpha)
+    p02<-rep(alpha*lambda[,2]*lambda[,2]/2,length(b02))
+    idbeta<-which(b02<=alpha*lambda[,2])
+    p02[idbeta]<-lambda[,2]*abs(b02[idbeta])-((b02[idbeta]*b02[idbeta])/2*alpha)
     
-    p12<-rep(alpha*lambda[3]*lambda[3]/2,length(b12))
-    idbeta<-which(b12<=alpha*lambda[3])
-    p12[idbeta]<-lambda[3]*abs(b12[idbeta])-((b12[idbeta]*b12[idbeta])/2*alpha)
->>>>>>> 988731d5c34e57a5b25fe660114a77eb65e373fb
-    
+    p12<-rep(alpha*lambda[,3]*lambda[,3]/2,length(b12))
+    idbeta<-which(b12<=alpha*lambda[,3])
+    p12[idbeta]<-lambda[,3]*abs(b12[idbeta])-((b12[idbeta]*b12[idbeta])/2*alpha)
+
     
     res<-res+sum(p01)+sum(p02)+sum(p12)
     
   }
   
   if(penalty=="scad"){
-    
-<<<<<<< HEAD
+
     p01<-rep((lambda[,1]^2)*(alpha+1)/2,length(b01))
     idbeta<-which(b01<=lambda[,1])
     p01[idbeta]<-lambda[,1]*abs(b01[idbeta])
@@ -102,26 +99,25 @@ idmlLikelihoodweibpena<-function(b0,np0,npar0,bfix0,fix0,c0,no0,ve010,ve020,ve12
     p12[idbeta]<-lambda[,3]*abs(b12[idbeta])
     idbeta<-which(abs(b12)<lambda[,3]*alpha)
     p12[idbeta]<-(2*alpha*lambda[,3]*abs(b12[idbeta])-b12[idbeta]^2-lambda[,3]^2)/(2*(alpha-1))
-=======
-    p01<-rep((lambda[1]^2)*(alpha+1)/2,length(b01))
-    idbeta<-which(b01<=lambda[1])
-    p01[idbeta]<-lambda[1]*abs(b01[idbeta])
-    idbeta<-which(abs(b01)<lambda[1]*alpha)
-    p01[idbeta]<-(2*alpha*lambda[1]*abs(b01[idbeta])-b01[idbeta]^2-lambda[1]^2)/(2*(alpha-1))
+
+    p01<-rep((lambda[,1]^2)*(alpha+1)/2,length(b01))
+    idbeta<-which(b01<=lambda[,1])
+    p01[idbeta]<-lambda[,1]*abs(b01[idbeta])
+    idbeta<-which(abs(b01)<lambda[,1]*alpha)
+    p01[idbeta]<-(2*alpha*lambda[,1]*abs(b01[idbeta])-b01[idbeta]^2-lambda[,1]^2)/(2*(alpha-1))
     
-    p02<-rep((lambda[2]^2)*(alpha+1)/2,length(b02))
-    idbeta<-which(b02<=lambda[2])
-    p02[idbeta]<-lambda[2]*abs(b02[idbeta])
-    idbeta<-which(abs(b02)<lambda[2]*alpha)
-    p02[idbeta]<-(2*alpha*lambda[2]*abs(b02[idbeta])-b02[idbeta]^2-lambda[2]^2)/(2*(alpha-1))
+    p02<-rep((lambda[,2]^2)*(alpha+1)/2,length(b02))
+    idbeta<-which(b02<=lambda[,2])
+    p02[idbeta]<-lambda[,2]*abs(b02[idbeta])
+    idbeta<-which(abs(b02)<lambda[,2]*alpha)
+    p02[idbeta]<-(2*alpha*lambda[,2]*abs(b02[idbeta])-b02[idbeta]^2-lambda[,2]^2)/(2*(alpha-1))
     
-    p12<-rep((lambda[3]^2)*(alpha+1)/2,length(b12))
-    idbeta<-which(b12<=lambda[3])
-    p12[idbeta]<-lambda[3]*abs(b12[idbeta])
-    idbeta<-which(abs(b12)<lambda[3]*alpha)
-    p12[idbeta]<-(2*alpha*lambda[3]*abs(b12[idbeta])-b12[idbeta]^2-lambda[3]^2)/(2*(alpha-1))
->>>>>>> 988731d5c34e57a5b25fe660114a77eb65e373fb
-    
+    p12<-rep((lambda[,3]^2)*(alpha+1)/2,length(b12))
+    idbeta<-which(b12<=lambda[,3])
+    p12[idbeta]<-lambda[,3]*abs(b12[idbeta])
+    idbeta<-which(abs(b12)<lambda[,3]*alpha)
+    p12[idbeta]<-(2*alpha*lambda[,3]*abs(b12[idbeta])-b12[idbeta]^2-lambda[,3]^2)/(2*(alpha-1))
+   
     
     res<-res+sum(p01)+sum(p02)+sum(p12)
   }
